7a8eacf4912c329e2b678183c1c279c47c5294c1
Jorge <jorgecorderozx@gmail.com>
2023-08-04 14:00:26 -0400

    Color ladder effect added

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let mainSection = document.getElementById("mainSection");
 let thankYouSection = document.getElementById("thankYouSection");
 let body = document.querySelector("body");
 let rateAgain = document.getElementById("rateAgain");
+let number;
 
 submitButton.addEventListener("click", function (e) {
     e.stopPropagation();
@@ -22,19 +23,39 @@ ratingButtons.forEach(function (rate) {
     rate.addEventListener("click", function (e) {
         e.stopPropagation();
         ratingOutput.innerHTML = rate.innerHTML;
+        number = ratingOutput.innerHTML;;
+        ratingColor();
     })
 })
 
-body.addEventListener("click", function(){
-    if(thankYouSection.style.display == "none"){
+body.addEventListener("click", function () {
+    if (thankYouSection.style.display == "none") {
         ratingOutput.innerHTML = ""
     }
-    
+    for (i = 0; i < ratingButtons.length; i++) {
+        ratingButtons[i].style.backgroundColor = "#262f38"
+        ratingButtons[i].style.color = "hsl(217, 12%, 63%)"
+    }
+
 })
 
-rateAgain.addEventListener("click", function(e){
+rateAgain.addEventListener("click", function (e) {
     e.stopPropagation();
     thankYouSection.style.display = "none";
     mainSection.style.display = "flex"
 })
 
+function ratingColor() {
+    parseInt(number);
+    for (i = 0; i < ratingButtons.length; i++) {
+        if (i < number) {
+            ratingButtons[i].style.backgroundColor = "hsl(217, 12%, 63%)"
+            ratingButtons[i].style.color = "hsl(0, 0%, 100%)"
+        }
+
+        else {
+            ratingButtons[i].style.backgroundColor = "#262f38"
+            ratingButtons[i].style.color = "hsl(217, 12%, 63%)"
+        }
+    }
+}
