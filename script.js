d549a781cd8a14944b30f9029cc911320273d638
Jorge <jorgecorderozx@gmail.com>
2023-08-04 11:46:55 -0400

    Rate again button added and StopPropagation methods

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ let ratingButtons = document.querySelectorAll(".ratingButton");
 let ratingOutput = document.getElementById("ratingOutput");
 let mainSection = document.getElementById("mainSection");
 let thankYouSection = document.getElementById("thankYouSection");
+let body = document.querySelector("body");
+let rateAgain = document.getElementById("rateAgain");
 
-submitButton.addEventListener("click", function () {
+submitButton.addEventListener("click", function (e) {
+    e.stopPropagation();
     if (ratingOutput.innerHTML == "") {
         alert("You must select a rate before submitting");
     }
@@ -16,8 +19,22 @@ submitButton.addEventListener("click", function () {
 })
 
 ratingButtons.forEach(function (rate) {
-    rate.addEventListener("click", function () {
+    rate.addEventListener("click", function (e) {
+        e.stopPropagation();
         ratingOutput.innerHTML = rate.innerHTML;
     })
+})
+
+body.addEventListener("click", function(){
+    if(thankYouSection.style.display == "none"){
+        ratingOutput.innerHTML = ""
+    }
+    
+})
+
+rateAgain.addEventListener("click", function(e){
+    e.stopPropagation();
+    thankYouSection.style.display = "none";
+    mainSection.style.display = "flex"
 })
 
